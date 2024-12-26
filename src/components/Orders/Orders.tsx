92a761765c96c6a04b75e18042a03dfc8d0fea3b
rafaapcode <rafaap2003@gmail.com>
2024-12-26 07:49:00 -0300

    feat: creating the baord component

--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -1,10 +1,59 @@
-
 function Orders() {
   return (
-    <div>
-
+    <div className="w-full max-w-[1216px] my-10 mx-auto flex gap-8">
+      <div className="p-4 flex-1 border border-[#ccc] rounded-2xl flex flex-col items-center">
+        <header className="p-2 text-base flex items-center gap-2">
+          <span>🕛</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+        <div className="flex flex-col w-full mt-6 gap-6">
+          <button type="button" className="bg-white flex flex-col justify-center items-center gap-1 w-full border border-[#ccc] h-32 rounded-lg">
+            <strong className="font-medium">Mesa 2</strong>
+            <span className="text-sm text-[#666]">2 itens</span>
+          </button>
+          <button type="button" className="bg-white flex flex-col justify-center items-center gap-1 w-full border border-[#ccc] h-32 rounded-lg">
+            <strong className="font-medium">Mesa 2</strong>
+            <span className="text-sm text-[#666]">2 itens</span>
+          </button>
+        </div>
+      </div>
+      <div className="p-4 flex-1 border border-[#ccc] rounded-2xl flex flex-col items-center">
+        <header className="p-2 text-base flex items-center gap-2">
+          <span>🕛</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+        <div className="flex flex-col w-full mt-6 gap-6">
+          <button type="button" className="bg-white flex flex-col justify-center items-center gap-1 w-full border border-[#ccc] h-32 rounded-lg">
+            <strong className="font-medium">Mesa 2</strong>
+            <span className="text-sm text-[#666]">2 itens</span>
+          </button>
+          <button type="button" className="bg-white flex flex-col justify-center items-center gap-1 w-full border border-[#ccc] h-32 rounded-lg">
+            <strong className="font-medium">Mesa 2</strong>
+            <span className="text-sm text-[#666]">2 itens</span>
+          </button>
+        </div>
+      </div>
+      <div className="p-4 flex-1 border border-[#ccc] rounded-2xl flex flex-col items-center">
+        <header className="p-2 text-base flex items-center gap-2">
+          <span>🕛</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+        <div className="flex flex-col w-full mt-6 gap-6">
+          <button type="button" className="bg-white flex flex-col justify-center items-center gap-1 w-full border border-[#ccc] h-32 rounded-lg">
+            <strong className="font-medium">Mesa 2</strong>
+            <span className="text-sm text-[#666]">2 itens</span>
+          </button>
+          <button type="button" className="bg-white flex flex-col justify-center items-center gap-1 w-full border border-[#ccc] h-32 rounded-lg">
+            <strong className="font-medium">Mesa 2</strong>
+            <span className="text-sm text-[#666]">2 itens</span>
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Orders
+export default Orders;
