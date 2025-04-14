558707de7be9932ab5b0116fee65fdacc7120063
rafaapcode <rafaap2003@gmail.com>
2025-04-14 17:22:41 -0300

    feat: begin the creation of the history table

--- src/components/historyTable/HistoryTable.tsx
+++ src/components/historyTable/HistoryTable.tsx
@@ -1,54 +1,63 @@
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Eye, Filter, Trash2 } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { Funnel } from "lucide-react";
 import { orders } from "./mockData";
 
 function HistoryTable() {
+  const table = useReactTable({
+    data: orders,
+    columns: [
+      { accessorKey: "table", header: () => <p className="text-[#333333] font-semibold">Mesa</p> },
+      { accessorKey: "date", header: () => <div className="flex gap-2 items-center text-[#333333] font-semibold">
+        <p>Data</p>
+        <Funnel size={14}/>
+      </div> },
+      { accessorKey: "name", header: () => <p className="text-[#333333] font-semibold">Nome</p> },
+      { accessorKey: "category", header: () => <p className="text-[#333333] font-semibold">Categoria</p> },
+      { accessorKey: "total", header: () => <p className="text-[#333333] font-semibold">Total</p> },
+      {accessorKey: 'id', header: () => <p className="text-[#333333] font-semibold">Ações</p>}
+    ],
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
-    <div className="w-full mt-4">
-      <div className="rounded-xl border bg-white overflow-hidden">
-        <Table>
-          <TableHeader className="bg-[#CCCCCC33]/20">
-            <TableRow>
-              <TableHead className="text-[#333333] font-semibold w-[5%] pl-4">Mesa</TableHead>
-              <TableHead className="w-[10%] text-ellipsis">
-                <div className="flex items-center gap-1 text-[#333333] font-semibold">
-                  Data
-                  <Filter className="h-4 w-4" />
-                </div>
-              </TableHead>
-              <TableHead className="text-[#333333] font-semibold text-left w-[25%]">Nome</TableHead>
-              <TableHead className="text-[#333333] font-semibold text-left w-[20%]">Categoria</TableHead>
-              <TableHead className="text-[#333333] font-semibold text-left flex-1">Total</TableHead>
-              <TableHead className="w-[10%] text-right text-[#333333] font-semibold pr-20">Ações</TableHead>
+    <div className="w-full mt-4 rounded-xl border bg-white overflow-y-auto max-h-full">
+      <Table>
+        <TableHeader className="bg-[#CCCCCC33]/20">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {
+                headerGroup.headers.map(header => (
+                  <TableHead key={header.id} className="px-4">
+                    {flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                ))
+              }
             </TableRow>
-          </TableHeader>
-          <TableBody>
-            {orders.map((order) => (
-              <TableRow key={order.id}>
-                <TableCell className="py-6 pl-4">{order.table}</TableCell>
-                <TableCell className="py-6">{order.date}</TableCell>
-                <TableCell className="py-6">{order.name}</TableCell>
-                <TableCell className="py-6">
-                  {order.category}
-                </TableCell>
-                <TableCell className="py-6">{order.total}</TableCell>
-                <TableCell className="text-right py-5 flex justify-center">
-                  <div className="flex gap-6 w-fit">
-                    <button className="text-gray-500 hover:text-gray-700">
-                      <Eye className="h-5 w-5" />
-                    </button>
-                    <button className="text-red-500 hover:text-red-700">
-                      <Trash2 className="h-5 w-5" />
-                    </button>
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+          ))}
+        </TableHeader>
+
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id}>
+              {
+                row.getAllCells().map(cell => (
+                  <TableCell key={cell.id} className="py-6 px-4">{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                ))
+              }
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
-  )
+  );
 }
 
 export default HistoryTable;
